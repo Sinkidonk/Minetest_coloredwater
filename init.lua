ffb81661212f2659a273fc118988bde22c8277da
Sinkidonk <aparys001@gmail.com>
2015-05-15 14:17:02 -0500

    added orange color

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 -- Project name: coloredwater
 -- Project purpose: I like the idea of haveing color water in minetest and now we can do just that.
--- Created/revised by: Sinkidonk on 5/13/15
+-- Created/revised by: Sinkidonk on 5/13/15 last revised on 5/15/15, revised by Sinkidonk
 
 
 
@@ -8,3 +8,5 @@
 dofile(minetest.get_modpath("coloredwater").."/green.lua")
 dofile(minetest.get_modpath("coloredwater").."/blue.lua")
 dofile(minetest.get_modpath("coloredwater").."/red.lua")
+dofile(minetest.get_modpath("coloredwater").."/orange.lua")
+
