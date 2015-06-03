0247d8f44c5171b5be583942c734408f3475426a
Sinkidonk <aparys001@gmail.com>
2015-06-03 00:26:46 -0500

    remove looptest.lua

--- init.lua
+++ init.lua
@@ -1,12 +1,12 @@
 -- Project name: coloredwater
 -- Project purpose: I like the idea of haveing color water in minetest and now we can do just that.
--- Created/revised by: Sinkidonk on 5/13/15 last revised on 5/17/15, revised by Sinkidonk
+-- Created/revised by: Sinkidonk on 5/13/15 last revised on 6/3/15, revised by Sinkidonk
 
 
 
 
---dofile(minetest.get_modpath("coloredwater").."/green.lua")
---dofile(minetest.get_modpath("coloredwater").."/blue.lua")
+dofile(minetest.get_modpath("coloredwater").."/green.lua")
+dofile(minetest.get_modpath("coloredwater").."/blue.lua")
 dofile(minetest.get_modpath("coloredwater").."/red.lua")
 dofile(minetest.get_modpath("coloredwater").."/orange.lua")
 dofile(minetest.get_modpath("coloredwater").."/aqua.lua")
@@ -16,4 +16,4 @@ dofile(minetest.get_modpath("coloredwater").."/magenta.lua")
 dofile(minetest.get_modpath("coloredwater").."/redviolet.lua")
 dofile(minetest.get_modpath("coloredwater").."/skyblue.lua")
 
-dofile(minetest.get_modpath("coloredwater").."/looptest.lua")
+
